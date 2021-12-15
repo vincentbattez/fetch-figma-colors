a4d3a86c79680f6c7de68c36aa9309bab9904a7c
Vincent Battez <vincent.battez.pro@gmail.com>
2021-12-15 11:16:22 -0500

    first commit

---
+++ Cellulaires et appareils mobiles - iPhone, Samsung, Pixel _ Fido_files/dossier sans titre/browser-update.js
@@ -0,0 +1,8 @@
+$buoop = {
+  // https://browser-update.org/customize.html
+  required: { e: -2, i: 11, f: -5, o: -5, s: -5, c: -5 },
+  reminderClosed: 48,
+  insecure: true,
+  api: 2018.09,
+  test: false
+};
